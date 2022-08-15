802b134fd9f2d8c9ed2c4f421d1ca021a4547501
fullstack300622 <108460728+fullstack300622@users.noreply.github.com>
2022-08-15 21:26:54 +0300

    Create polyMain.java

---
+++ polyMain.java
@@ -0,0 +1,40 @@
+public class Main {
+
+    public static void main(String[] args) {
+//        System.out.println("Hello world!");
+//        Animal animal = new Animal();
+//        Animal cat = new Cat();
+//        Animal dog = new Dog();
+//
+//        animal.eat();
+//        cat.eat();
+//        dog.eat();
+
+//        String[] myString = new String[5];
+//
+//        Animal[] animalsArray = new Animal[4];
+//
+//        Animal justAnimal = new Animal();
+//        Animal cat = new Cat();
+//        Animal dog = new Dog();
+//        Cat anotherCat = new Cat();
+//        Jiraffa jiraffa = new Jiraffa();
+//        Animal anotherAnimal = new Animal();
+//        animalsArray[0]=cat;
+//        animalsArray[1]=dog;
+//        animalsArray[2]=anotherCat;
+//        animalsArray[3]=anotherAnimal;
+//
+//        for (int i = 0; i <animalsArray.length ; i++) {
+//            animalsArray[i].eat();
+//        }
+
+//        Person person = new Person("Shlomo");
+//        person.playWithAnimal(justAnimal);
+//        person.playWithAnimal(cat);
+//        person.playWithAnimal(dog);
+//        person.playWithAnimal(anotherCat);
+//        person.playWithAnimal(jiraffa);
+
+    }
+}
