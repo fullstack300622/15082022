355aa37896d21417c6070559ecc555a219e86541
fullstack300622 <108460728+fullstack300622@users.noreply.github.com>
2022-08-15 21:28:04 +0300

    Create polyDog.java

---
+++ polyDog.java
@@ -0,0 +1,12 @@
+public class Dog extends Animal{
+
+    @Override
+    public void eat() {
+        System.out.println("eating Bonzo");
+    }
+
+    @Override
+    public void play() {
+        System.out.println("Dog playing ball");
+    }
+}
