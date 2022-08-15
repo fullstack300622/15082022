86cc9a1568e62237bd32343bf1accc100c7bdde6
fullstack300622 <108460728+fullstack300622@users.noreply.github.com>
2022-08-15 21:28:39 +0300

    Create abstractMain.java

---
+++ abstractMain.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+//        Food f1 = new Food();
+//        Bamba bamba = new Bamba();
+        Food bamba = new Bamba();
+        bamba.smell();
+    }
+}
