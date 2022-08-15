f6f6b1bf13ac64e806947e4d821c258f36fe3bb3
fullstack300622 <108460728+fullstack300622@users.noreply.github.com>
2022-08-15 21:28:21 +0300

    Create polyAnimal.java

---
+++ polyAnimal.java
@@ -0,0 +1,9 @@
+public class Animal {
+    public void eat(){
+        System.out.println("Animal eating");
+    }
+
+    public void play(){
+        System.out.println("Animal playing");
+    }
+}
