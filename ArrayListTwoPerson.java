1d4e14380cac914fad63eaf11989983cd264a0c1
fullstack300622 <108460728+fullstack300622@users.noreply.github.com>
2022-08-15 21:30:53 +0300

    Create ArrayListTwoPerson.java

---
+++ ArrayListTwoPerson.java
@@ -0,0 +1,46 @@
+public class Person extends Soul{
+    private int age;
+    private String name;
+
+    public Person(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void play() {
+        System.out.println(this.name + " Playing");
+    }
+
+//    public String toString(){
+//        return ("age "+this.age+ " name "+this.name);
+//    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public String goTo() {
+        return ("I am go to Hell");
+    }
+}
