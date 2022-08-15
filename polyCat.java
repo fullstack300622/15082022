39b5b724c82bf0a4f7f2c474bc84a52f28a612b2
fullstack300622 <108460728+fullstack300622@users.noreply.github.com>
2022-08-15 21:27:47 +0300

    Create polyCat.java

---
+++ polyCat.java
@@ -0,0 +1,12 @@
+public class Cat extends Animal{
+
+
+    public void eat() {
+        System.out.println("Eating Wiskas");
+    }
+
+    @Override
+    public void play() {
+        System.out.println("Cat playing with rat");
+    }
+}
