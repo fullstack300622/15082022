99fecab8f68c3a508803b12ecbe7797b4474c01d
fullstack300622 <108460728+fullstack300622@users.noreply.github.com>
2022-08-15 21:29:16 +0300

    Create abstractFood.java

---
+++ abstractFood.java
@@ -0,0 +1,11 @@
+public abstract class Food {
+
+    public void smell(){
+        System.out.println("food smell");
+    }
+
+    public abstract void putIngredients();
+
+    public abstract void addColor();
+
+}
